b1c8b25f2b1686a63944cfd5937d246710e0fa1b
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2018-10-19 19:28:12 -0700

    add flashcards in 2 rows and 3 columns, use jquery logic to make them clickable and toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
 $(document).ready(function(){
-  $(".clickable").click(function(){
-    $("p#javascriptDefinition").toggle();
-    $("h1#javascript").toggle();
+// Javascript Flashcard FE logic
+  $("#javascript-clickable").click(function(){
+    $("p#javascriptDefinition").slideToggle();
+    $("h1#javascript").slideToggle();
+  });
 
+// Operators flashcard FE Logic
+  $("#operators-clickable").click(function(){
+    $("p#operatorsDefinition").slideToggle();
+    $("h1#operators").slideToggle();
   })
+
+// Variables flashcard FE logic
+  $("#variables-clickable").click(function(){
+    $("h1#variables").slideToggle();
+    $("p#variablesDefinition").slideToggle();
+  })
+
+// Variable naming conventions flashcard FE Logic
+  $("#naming-clickable").click(function(){
+    $("h1#naming-conventions").slideToggle();
+    $("p#namingDefinition").slideToggle();
+  })
+
+//functions flashcard FE logic
+  $("#functions-clickable").click(function(){
+    $("h1#functions").slideToggle();
+    $("p#functionsDefinition").slideToggle();
+  })
+
+//jQuery flashcard FE logic
+  $("#jquery-clickable").click(function(){
+    $("h1#jquery").slideToggle();
+    $("p#jqueryDefinition").slideToggle();
+  })
+
+
+
+
+
 })
